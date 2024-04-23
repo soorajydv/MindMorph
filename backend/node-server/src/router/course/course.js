10032ae018368f1e2ca4c84391747c428495f133
Narayan Gautam <ng4111894@gmail.com>
2024-04-23 23:03:59 +0545

    ReAdd courseChoice and get course by id and get courses, deleted during last merge with @sooraj

--- backend/node-server/src/router/course/course.js
+++ backend/node-server/src/router/course/course.js
@@ -7,7 +7,17 @@ const {
 } = require('../../controller/course/course');
 const upload = require('../../middleware/fileUpload');
 
-// router.post('/id',getCourse);
+const {
+  getLatestCourse,
+  getTrendingCourse,
+} = require('../controller/course/courseChoice');
+
+// Course Choice
+router.get('/latest', getLatestCourse);
+router.get('/trending', getTrendingCourse);
+
+router.get('', getAllCourse);
+router.get('/:id', getCourse);
 router.post('/', upload('courseThumbnail', 'thumbnail'), createCourse);
 router.patch('/:id', upload('courseThumbnail', 'thumbnail'), updateCourse);
 router.delete('/:id', deleteCourse);
