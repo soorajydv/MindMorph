0db272c82b13a2bd991bf08c590becf1f99cfdb5
Ydv Sooraj <soorajydv9@gmail.com>
2024-06-24 23:48:50 +0545

    refactor

--- backend/node-server/src/index.js
+++ backend/node-server/src/index.js
@@ -1,4 +1,6 @@
+//import required libraries/packages
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const passport = require('passport');
 const session = require('express-session');
@@ -8,12 +10,11 @@ const { Pool } = require('pg');
 require('ejs');
 require('./conf/facebookPassport')(passport);
 const http = require('http');
-const cors = require('cors')
+const cors = require('cors');
 const { Server } = require("socket.io");
 
 // Import API Routers
 const authRouter = require('./router/auth');
-const home = require('./controller/home');
 const courseRouter = require('./router/course/course');
 const socialHandleRouter = require('./router/socialHandle');
 const courseDomainRouter = require('./router/course/courseDomain');
@@ -24,40 +25,35 @@ const cartRouter = require('./router/cart');
 const enrollCourseRouter = require('./router/enrolledCourse');
 const oauthRouter = require('./router/oauth');
 const oauthFbRouter = require('./router/oauthFb');
-const chat = require('./router/chat');
 
-const bodyParser = require('body-parser');
+// Import Controllers
+const initializeSocket = require('./socket/chat/socket');
+const home = require('./controller/home');
+
 const app = express();
+app.use(cors());
+
+// Websocket
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+const io = new Server(server);
+initializeSocket(io);
 
-const initializeSocket = require('./socket/socket');
-initializeSocket(server); // Pass the server instance here
-
-
-
-app.use(bodyParser.json());
+//implement bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
+//Postgres db connection
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL // Replace with your PostgreSQL connection string
 });
 
+//Session handling
 app.use(
   session({
     store: new pgSession({
       pool, tableName: "session" // Optionally, specify the name of the session table
     }),
-    secret: 'keyboard cat',
+    secret: process.env.SESSION_SECRET || 'keyboard cat',
     resave: false,
     saveUninitialized: false
   })
@@ -72,7 +68,7 @@ app.use('/media', express.static('media'));
 app.use('/public', express.static("public"));
 
 // Use API Routers
-app.use('/', authRouter);
+app.use('/auth', authRouter);
 app.use('/', home);
 app.use('/course', courseRouter);
 app.use('/socialHandle', socialHandleRouter);
@@ -84,7 +80,7 @@ app.use('/cart', cartRouter);
 app.use('/enroll', enrollCourseRouter);
 app.use('/', oauthRouter);
 app.use('/', oauthFbRouter);
-app.use('/', chat);
+
 
 // The 404 Route
 app.use('*', function (req, res, next) {
--- backend/node-server/src/router/auth.js
+++ backend/node-server/src/router/auth.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const signup = require('../controller/auth/signup');
-const login = require('../controller/auth/login');
 const upload = require('../middleware/fileUpload');
+const login = require('../controller/auth/login');
+const logout = require('../controller/auth/logout')
 
 router.post('/signup', upload('userAvatar', 'avatar'), signup);
 router.post('/login', login);
+router.get('/logout',logout);
 
 module.exports = router;
