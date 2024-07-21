40fc6a617487f3b36cba63ac392aa2aab2bb3b9d
Ydv Sooraj <soorajydv9@gmail.com>
2024-07-21 17:55:47 +0545

    add swiper

--- backend/node-server/src/controller/swiper.js
+++ backend/node-server/src/controller/swiper.js
@@ -3,16 +3,19 @@ const prisma = require('../../prisma/prisma')
 const uploadSwiper = async(req,res)=>{
     const imagePath = req.file.path.replaceAll('\\', '/'); // For windwos device
 
+    console.log('Image path: ',imagePath);
+
     try{
         const swiper =  await prisma.swiper.create({
             data:{
-                swiperPath:imagePath
+                image:imagePath
             }
-        })
+        });
         if(swiper){
-            return res.status(500).json({ message: 'swipers found' });
+            return res.status(201).json({ message: 'Swiper Added' });
         }
     }catch (error) {
+        console.log('Error: ',error);
         return res.status(500).json({ message: 'Internal Server Error' });
     }
     
@@ -28,8 +31,8 @@ const getASwiper = async (req, res) => {
             }
         })
         if (swiper) {
-            res.status(500).json({ message: 'A swiper found' });
-            return swiper
+           return  res.status(500).json({ message: 'A swiper found' , data:swiper});
+            
         }
     } catch (error) {
         return res.status(500).json({ message: 'Internal Server Error' });
@@ -39,12 +42,16 @@ const getASwiper = async (req, res) => {
 const getAllSwiper = async (req, res) => {
     try {
         const swipers = await prisma.swiper.findMany({
-            select: { isVisible: true }
+            where: { isVisible: true },
+            select:{
+                id:true,
+                image:true
+            }
         })
-        if (swipers) {
-            res.status(500).json({ message: 'swipers found' });
-            return swipers
-        }
+        
+    return  res.status(500).json(swipers);
+           
+        
     } catch (error) {
         return res.status(500).json({ message: 'Internal Server Error' });
     }
@@ -55,13 +62,17 @@ const changeVisiblity =  async(req,res)=>{
     const id = parseInt(req.params.id)
 
     try {
-        const swiper = await prisma.swiper.update({
-            where: { id },
-            data: { isVisible: !swiper?.isVisible }, // Update with boolean negation
+        const swiper = await prisma.swiper.findFirst({
+            where: { id }
         });
-        if (swiper) {
+
+        const result = await prisma.swiper.update({
+            where: { id },
+            data: { isVisible: !swiper.isVisible }, // Update with boolean negation
+        });
+        if (result) {
             // Success message (optional)
-            return res.status(200).json({ message: 'Visibility toggled successfully' });
+            return res.status(200).json({ message: 'Visibility Changed successfully' });
           } else {
             // Handle case where swiper not found
             return res.status(404).json({ message: 'Swiper not found' });
@@ -81,7 +92,7 @@ const deleteSwipper = async (req, res) => {
         try {
             const swiper = await prisma.swiper.delete({ where: { id: id } })
             if (swiper) {
-                return res.status(500).json({ message: 'swiper deleted' });
+                return res.status(500).json({ message: 'Swiper Deleted' });
             }
         } catch (error) {
             return res.status(500).json({ message: 'Internal Server Error' });
--- backend/node-server/src/router/swiper.js
+++ backend/node-server/src/router/swiper.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const upload = require('../middleware/fileUpload');
 const {uploadSwiper,getASwiper,getAllSwiper,changeVisiblity,deleteSwipper} = require('../controller/swiper')
 
-router.post('/', upload('swiper', 'swiper'), uploadSwiper);
+router.post('/', upload('swiper', 'image'), uploadSwiper);
 router.patch('/:id',changeVisiblity)
 router.get('/:id',getASwiper);
 router.get('/',getAllSwiper)
